a2f2ea2c92efc15e66af43619dbedc0610aeaa65
GeonHyeongKim <rjsgud1000@gmail.com>
2026-03-27 13:59:53 +0900

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/test/java/com/ownlife/OwnlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ownlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
