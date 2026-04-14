b6d9f6b94dcbabc2948ef5d43338cdcedfe66602
HyeonWoo <poindexter7293@gmail.com>
2026-04-14 11:25:22 +0900

    서버 2중대 연결

--- src/test/java/com/ownlife/OwnlifeApplicationTests.java
+++ src/test/java/com/ownlife/OwnlifeApplicationTests.java
@@ -1,7 +1,14 @@
 package com.ownlife;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
 
 @SpringBootTest
 class OwnlifeApplicationTests {
@@ -9,5 +16,4 @@ class OwnlifeApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
